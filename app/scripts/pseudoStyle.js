9b7ace7bdc84841f67c23131d37889e7e3b27ea6
Andrey Karamnoff <karamnoff@gmail.com>
2016-10-15 09:24:15 +0300

    :sparkles: (scripts) Add pseudoStyle

---
+++ app/scripts/pseudoStyle.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const UID = {
+	current: 0,
+	getNew() {
+		this.current++;
+		return this.current;
+	}
+};
+
+HTMLElement.prototype.pseudoStyle = function (element, prop, value) {
+	const self = this;
+	const sheetId = 'pseudoStyles';
+	const head = document.head || document.getElementsByTagName('head')[0];
+	const sheet = document.getElementById(sheetId) || document.createElement('style');
+	sheet.id = sheetId;
+	const className = 'pseudoStyle' + UID.getNew();
+
+	self.className += ' ' + className;
+
+	sheet.innerHTML += ' .' + className + ':' + element + '{' + prop + ':' + value + '}';
+	head.appendChild(sheet);
+	return this;
+};
+
+export default UID;
