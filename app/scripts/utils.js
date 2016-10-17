bf2853980b7bb399e9b6c3b6b87329e8bc1106bc
Andrey Karamnoff <karamnoff@gmail.com>
2016-10-17 13:42:34 +0300

    :fire: (scripts/utils.js) Remove unused methods from utils object

--- app/scripts/utils.js
+++ app/scripts/utils.js
@@ -2,9 +2,9 @@
 
 const utils = {
 	/**
-	 * Записывает в св-во value элемента текущую дату
-	 * @param name
-	 */
+	* Записывает в св-во value элемента текущую дату
+	* @param name
+	*/
 	setInputDate(name) {
 		const elem = document.querySelector(name);
 		const date = new Date();
@@ -23,31 +23,6 @@ const utils = {
 		formattedDate = year + '-' + month + '-' + day;
 
 		elem.value = formattedDate;
-	},
-	/**
-	 * Выполняет функцию fn не чаще одного раза в указанный в параметре delay период,
-	 * даже если она будет вызвана неоднократно в течение указанного периода
-	 * @param {Function} fn
-	 * @param {Number} delay
-	 * @return {Function}
-	 */
-	throttle(fn, delay) {
-		let thisMoment = 0;
-		return function () {
-			if (Date.now() - thisMoment > delay) {
-				fn();
-				thisMoment = Date.now();
-			}
-		};
-	},
-	/**
-	 * Выполняет глубокое копирование объекта
-	 * и возвращает его клон
-	 * @param {Object} obj
-	 * @return {*}
-	 */
-	cloneObj(obj) {
-		return JSON.parse(JSON.stringify(obj));
 	}
 };
 
